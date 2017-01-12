8d6e7855de33893ec426f3165de2f12256c44940
Christian <christiannguyendinh@yahoo.com>
2017-01-12 17:30:23 -0500

    Added showing all table names for java sqlite script

--- sqlite_stuff/sqlite_db_connection.java
+++ sqlite_stuff/sqlite_db_connection.java
@@ -21,12 +21,25 @@ public class ServerConnection {
             connection = DriverManager.getConnection("jdbc:sqlite:<PATH TO DB>");
             Statement statement = connection.createStatement();
             statement.setQueryTimeout(10);
-            //statement.executeUpdate("INSERT INTO testing (name) VALUES (\"Bawb\")");
 
+            // Example Queries
+
+            //statement.executeUpdate("INSERT INTO testing (name) VALUES (\"Bawb\")");
             ResultSet rs = statement.executeQuery("SELECT * FROM testing");
             while(rs.next()) {
                 System.out.print("id : " + rs.getString("id") + " | ");
                 System.out.println("name : " + rs.getString("name"));
+            }
+
+            // GET TABLE NAMES
+
+            // Search for all types: tables, views, aliases, etc.
+            // rs = connection.getMetaData().getTables(null, null, "%", null);
+            // Search for all and only 'TABLES'
+            rs = connection.getMetaData().getTables(null, null, null, new String[] {"TABLE"});
+            while(rs.next()) {
+                // sqlite_sequence is created when a table is created and uses AUTOINCREMENT
+                System.out.println(rs.getString("TABLE_NAME"));
             }
 
         } catch (SQLException e) {
