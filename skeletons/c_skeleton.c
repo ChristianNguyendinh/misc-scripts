f55c891a052bc319e3a20bcbf42cf575f6c00545
Christian <christiannguyendinh@yahoo.com>
2017-08-05 15:39:34 -0400

    we may want to add more substance to these skeletons

--- skeletons/c_skeleton.c
+++ skeletons/c_skeleton.c
@@ -1,15 +1,23 @@
 /*
-gcc c_skeleton.c -o <executable_name>.x
+gcc c_skeleton.c -o <executable_name>
+./<executable_name>
+
+(or whatever other c compiler)
 */
 
 #include <stdio.h>
 
+void hello(char str[]) {
+	printf("%s\n", str);
+}
+
 int main(void) {
 	int i = 55;
-
 	i += 10;
 
-	printf("i is %d", i);
+	printf("i is %d\n", i);
+
+	hello("asdf");
 
 	return 0;
 }
